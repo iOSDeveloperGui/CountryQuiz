22aa1990c531bc90a23123c81684a27f57794b9d
iOSDeveloperGui <guilhermesilvadias535@gmail.com>
2025-08-11 19:54:56 -0300

    Initial Commit

---
+++ FunFlag/FunFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunFlagApp.swift
+//  FunFlag
+//
+//  Created by iOS Developer on 11/08/25.
+//
+
+import SwiftUI
+
+@main
+struct FunFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
